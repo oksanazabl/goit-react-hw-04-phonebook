f6556e757eca8f1bb7ac5d149d294f02c4ad537b
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-05-08 12:42:06 +0300

    fix

--- src/components/PhonebookContatcs/PhonebookContatcs.jsx
+++ src/components/PhonebookContatcs/PhonebookContatcs.jsx
@@ -1,35 +1,39 @@
 import PropTypes from 'prop-types';
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 import css from './PhonebookContatcs.module.css';
 
 const PhonebookContacts = ({ contacts, onDeleteContact }) => {
-const handleDeleteContact = useCallback((id) => {
-onDeleteContact(id);
-}, [onDeleteContact]);
+  // const handleDeleteContact = useCallback((id) => {
+  // onDeleteContact(id);
+  // }, [onDeleteContact]);
 
-return (
-<ul>
-{contacts.map(({ id, name, number }) => (
-<li className={css.contact_item} key={id}>
-{name}: {number}
-<button className={css.button} type="button" onClick={() => handleDeleteContact(id)}>
-Delete Contact
-</button>
-</li>
-))}
-</ul>
-);
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.contact_item} key={id}>
+          {name}: {number}
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete Contact
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 PhonebookContacts.propTypes = {
-contacts: PropTypes.arrayOf(
-PropTypes.shape({
-id: PropTypes.string.isRequired,
-name: PropTypes.string.isRequired,
-number: PropTypes.string.isRequired
-})
-).isRequired,
-onDeleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default PhonebookContacts;
